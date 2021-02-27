150b94aeea659fabe197e1e0aebb9fe80ab43998
Matthias Specht <sylence@btbsoft.org>
2021-02-27 14:14:49 +0100

    Enable code coverage check in CI

---
+++ .github/workflows/check-code-coverage.ps1
@@ -0,0 +1,16 @@
+param(
+    [int] $threshold = 100,
+    [String] $resultFolder
+)
+
+Get-ChildItem $resultFolder -Filter *.xml -Recurse | ForEach-Object {
+    Write-Host "Checking code coverage results in '$($_)' with a threshold of $($threshold)"
+    $xml = [xml](Get-Content -Path $_)
+    
+    $lines = [double]::Parse( (Select-Xml -Xml $xml -XPath "//coverage/@line-rate"), [cultureinfo] 'en-US' ) * 100
+    $branches = [double]::Parse( (Select-Xml -Xml $xml -XPath "//coverage/@branch-rate"), [cultureinfo] 'en-US' ) * 100
+
+    if ( $lines -lt $threshold || $branches -lt $threshold ) {
+        Write-Error "Code coverage too low. Lines: $($lines)%, branches: $($branches)%, threshold: $($threshold)%"
+    }
+}
