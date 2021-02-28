3c462646336988f370fd16c74e6849159566c008
Matthias Specht <sylence@btbsoft.org>
2021-02-28 09:37:41 +0100

    Use correct return code in integration test script

--- .github/workflows/run-integration-tests.ps1
+++ .github/workflows/run-integration-tests.ps1
@@ -3,6 +3,8 @@ param(
     [String] $baseFolder
 )
 
+$failedTests = $false
+
 $failingFolder = "$($baseFolder)/Failing"
 Get-ChildItem $failingFolder -Filter *.yml | ForEach-Object {
     $projectFile = [System.IO.Path]::ChangeExtension($_, ".proj");
@@ -10,6 +12,7 @@ Get-ChildItem $failingFolder -Filter *.yml | ForEach-Object {
     & dotnet $exe --config $($_) --project $($projectFile) > $null 2>&1
     if ( $LASTEXITCODE -eq 0 ) {
         Write-Error "Expected $($_) to fail but it did not"
+        $failedTests = $true
     }
 }
 
@@ -20,5 +23,13 @@ Get-ChildItem $succeedingFolder -Filter *.yml | ForEach-Object {
     & dotnet $exe --config $($_) --project $($projectFile) > $null 2>&1
     if ( $LASTEXITCODE -ne 0 ) {
         Write-Error "Expected $($_) to succeed but it did not"
+        $failedTests = $true
     }
 }
+
+if ( $failedTests ) {
+    Write-Error "One or more tests have failed"
+    Exit 1
+}
+Write-Host "All test cases successfully run"
+Exit 0
