9e1150d1d4bec3f519933afdc56a652a5c6f3c0c
Matthias Specht <sylence@btbsoft.org>
2021-02-28 09:27:00 +0100

    Add integration tests to CI

---
+++ .github/workflows/run-integration-tests.ps1
@@ -0,0 +1,24 @@
+param(
+    [String] $exe,
+    [String] $baseFolder
+)
+
+$failingFolder = "$($baseFolder)/Failing"
+Get-ChildItem $failingFolder -Filter *.yml | ForEach-Object {
+    $projectFile = [System.IO.Path]::ChangeExtension($_, ".proj");
+
+    & $exe --config $($_) --project $($projectFile) > $null 2>&1
+    if ( $LASTEXITCODE -eq 0 ) {
+        Write-Error "Expected $($_) to fail but it did not"
+    }
+}
+
+$succeedingFolder = "$($baseFolder)/Succeeding"
+Get-ChildItem $succeedingFolder -Filter *.yml | ForEach-Object {
+    $projectFile = [System.IO.Path]::ChangeExtension($_, ".proj");
+
+    & $exe --config $($_) --project $($projectFile) > $null 2>&1
+    if ( $LASTEXITCODE -ne 0 ) {
+        Write-Error "Expected $($_) to succeed but it did not"
+    }
+}
