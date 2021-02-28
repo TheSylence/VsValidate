12a0612e8633c68d2aa7c5047294b0c312d31a2b
Matthias Specht <sylence@btbsoft.org>
2021-02-28 16:00:00 +0100

    Use correct search path for code coverage results

--- .github/workflows/upload-code-coverage.ps1
+++ .github/workflows/upload-code-coverage.ps1
@@ -2,7 +2,7 @@ param(
     [String]$token="1234567"
 )
 
-Get-ChildItem ./CoverageResults -Filter *.xml -Recurse | ForEach-Object {
+Get-ChildItem ./VsValidate.Tests/TestResults -Filter *.xml -Recurse | ForEach-Object {
     java -jar ./codacy-test-reporter.jar report -l CSharp -r $_ -t $token --partial
 }
 
