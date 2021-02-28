c34b16b896a322a491c699e7be708b1ecd956653
Matthias Specht <sylence@btbsoft.org>
2021-02-28 16:39:10 +0100

    Remove obsolete script

--- .github/workflows/upload-code-coverage.ps1
+++
@@ -1,9 +0,0 @@
-param( 
-    [String]$token="1234567"
-)
-
-Get-ChildItem ./VsValidate.Tests/TestResults -Filter *.xml -Recurse | ForEach-Object {
-    java -jar ./codacy-test-reporter.jar report -l CSharp -r $_ -t $token --partial
-}
-
-java -jar ./codacy-test-reporter.jar final -t $token
