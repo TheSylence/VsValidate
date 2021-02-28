81e4545baedd23d4e471423bfa406a5d0b6de55d
Matthias Specht <sylence@btbsoft.org>
2021-02-28 09:32:51 +0100

    Use correct assembly during integration tests

--- .github/workflows/run-integration-tests.ps1
+++ .github/workflows/run-integration-tests.ps1
@@ -7,7 +7,7 @@ $failingFolder = "$($baseFolder)/Failing"
 Get-ChildItem $failingFolder -Filter *.yml | ForEach-Object {
     $projectFile = [System.IO.Path]::ChangeExtension($_, ".proj");
 
-    & $exe --config $($_) --project $($projectFile) > $null 2>&1
+    & dotnet $exe --config $($_) --project $($projectFile) > $null 2>&1
     if ( $LASTEXITCODE -eq 0 ) {
         Write-Error "Expected $($_) to fail but it did not"
     }
@@ -17,7 +17,7 @@ $succeedingFolder = "$($baseFolder)/Succeeding"
 Get-ChildItem $succeedingFolder -Filter *.yml | ForEach-Object {
     $projectFile = [System.IO.Path]::ChangeExtension($_, ".proj");
 
-    & $exe --config $($_) --project $($projectFile) > $null 2>&1
+    & dotnet $exe --config $($_) --project $($projectFile) > $null 2>&1
     if ( $LASTEXITCODE -ne 0 ) {
         Write-Error "Expected $($_) to succeed but it did not"
     }
