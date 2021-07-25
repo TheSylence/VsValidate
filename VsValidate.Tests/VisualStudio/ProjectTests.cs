56f06d530cb09a409046a886c47eebfcdfc3f3e1
Matthias Specht <sylence@btbsoft.org>
2021-07-25 13:37:33 +0200

    Merge partial class

--- VsValidate.Tests/VisualStudio/ProjectTests.cs
+++ VsValidate.Tests/VisualStudio/ProjectTests.cs
@@ -1,10 +1,52 @@
-﻿using VsValidate.VisualStudio;
+﻿using System.Linq;
+using System.Xml.Linq;
+using VsValidate.VisualStudio;
 using Xunit;
 
 namespace VsValidate.Tests.VisualStudio
 {
 	public partial class ProjectTests
 	{
+		[Fact]
+		public void ConditionShouldBeAppliedToItemGroupWhenNestedInChooseElement()
+		{
+			// Arrange
+			var xml = new ProjectBuilder()
+				.BeginChoose()
+				.WithWhen("variable == value")
+				.WithItemGroup()
+				.WithPackageReference("package", "1.2")
+				.BuildXml();
+
+			// Act
+			var sut = new Project(xml);
+
+			// Assert
+			var reference = Assert.Single(sut.PackageReferences);
+			Assert.NotNull(reference!.Condition);
+			Assert.Equal("variable == value", reference.Condition!.Expression);
+		}
+
+		[Fact]
+		public void ConditionShouldBeAppliedToPropertyGroupWhenNestedInChooseElement()
+		{
+			// Arrange
+			var xml = new ProjectBuilder()
+				.BeginChoose()
+				.WithWhen("variable == value")
+				.WithPropertyGroup()
+				.WithProperty("prop", "value")
+				.BuildXml();
+
+			// Act
+			var sut = new Project(xml);
+
+			// Assert
+			var propertyGroup = Assert.Single(sut.PropertyGroups);
+			Assert.NotNull(propertyGroup!.Condition);
+			Assert.Equal("variable == value", propertyGroup.Condition!.Expression);
+		}
+
 		[Fact]
 		public void FileNameShouldBeSet()
 		{
@@ -22,6 +64,94 @@ namespace VsValidate.Tests.VisualStudio
 		}
 
 		[Fact]
+		public void PackageReferenceShouldBeReadWhenStoredInMultipleGroups()
+		{
+			// Arrange			
+			var xml = new ProjectBuilder()
+				.WithItemGroup()
+				.WithPackageReference("Package.Name", "1.2.3.4-pre5")
+				.WithItemGroup()
+				.WithPackageReference("Package.Two", "0.1")
+				.BuildXml();
+
+			// Act
+			var sut = new Project(xml);
+
+			// Assert
+			Assert.Contains(sut.PackageReferences, x => x.Name == "Package.Name" && x.Version == "1.2.3.4-pre5");
+			Assert.Contains(sut.PackageReferences, x => x.Name == "Package.Two" && x.Version == "0.1");
+		}
+
+		[Fact]
+		public void PackageReferenceShouldContainConditionOfItemGroup()
+		{
+			// Arrange
+			var xml = new ProjectBuilder()
+				.WithItemGroup("condition-value")
+				.WithPackageReference("Package.Name", "1.2.3.4-pre5")
+				.BuildXml();
+
+			// Act
+			var sut = new Project(xml);
+
+			// Assert
+			var package = Assert.Single(sut.PackageReferences);
+			Assert.NotNull(package!.Condition);
+			Assert.Equal("condition-value", package.Condition!.Expression);
+		}
+
+		[Fact]
+		public void PackageReferenceShouldHaveCorrectVersionWhenNestedAsTag()
+		{
+			// Arrange
+			var xml = new ProjectBuilder()
+				.WithItemGroup()
+				.WithPackageReference("Package.Name", "1.0", true)
+				.BuildXml();
+
+			// Act
+			var sut = new Project(xml);
+
+			// Assert
+			Assert.Contains(sut.PackageReferences, x => x.Name == "Package.Name" && x.Version == "1.0");
+		}
+
+		[Fact]
+		public void PackageReferenceShouldHaveEmptyVersionWhenNotSpecified()
+		{
+			// Arrange
+			var xml = new ProjectBuilder()
+				.WithItemGroup()
+				.WithPackageReference("Package.Name", null)
+				.BuildXml();
+
+			// Act
+			var sut = new Project(xml);
+
+			// Assert
+			Assert.Contains(sut.PackageReferences, x => string.IsNullOrEmpty(x.Version));
+		}
+
+		[Fact]
+		public void PackageReferencesShouldBeReadWhenStoredInSingleGroup()
+		{
+			// Arrange
+			var xml = new ProjectBuilder()
+				.WithItemGroup()
+				.WithPackageReference("Package.Name", "1.2.3.4-pre5")
+				.BuildXml();
+
+			// Act
+			var sut = new Project(xml);
+
+			// Assert
+			var package = Assert.Single(sut.PackageReferences);
+			Assert.Equal("Package.Name", package!.Name);
+			Assert.Equal("1.2.3.4-pre5", package.Version);
+			Assert.Null(package.Condition);
+		}
+
+		[Fact]
 		public void SdkShouldBeRead()
 		{
 			// Arrange
@@ -33,6 +163,207 @@ namespace VsValidate.Tests.VisualStudio
 
 			// Assert
 			Assert.Equal("Sdk.Name", sut.Sdk);
+		}
+	}
+
+	public partial class ProjectTests
+	{
+		[Fact]
+		public void ProjectReferenceShouldBeReadWhenStoredInMultipleGroups()
+		{
+			// Arrange
+			var xml = new ProjectBuilder()
+				.WithItemGroup()
+				.WithProjectReference("project1")
+				.WithItemGroup()
+				.WithProjectReference("project2")
+				.BuildXml();
+
+			// Act
+			var sut = new Project(xml);
+
+			// Assert
+			Assert.Contains(sut.ProjectReferences, x => x.Name == "project1");
+			Assert.Contains(sut.ProjectReferences, x => x.Name == "project2");
+		}
+
+		[Fact]
+		public void ProjectReferenceShouldBeReadWhenStoredInSingleGroup()
+		{
+			// Arrange
+			var xml = new ProjectBuilder()
+				.WithItemGroup()
+				.WithProjectReference("project/path.file")
+				.BuildXml();
+
+			// Act
+			var sut = new Project(xml);
+
+			// Assert
+			var project = Assert.Single(sut.ProjectReferences);
+			Assert.Equal("project/path.file", project!.Name);
+			Assert.Null(project.Condition);
+		}
+
+		[Fact]
+		public void ProjectReferenceShouldContainConditionOfItemGroup()
+		{
+			// Arrange
+			var xml = new ProjectBuilder()
+				.WithItemGroup("condition-value")
+				.WithProjectReference("project/path.file")
+				.BuildXml();
+
+			// Act
+			var sut = new Project(xml);
+
+			// Assert
+			var project = Assert.Single(sut.ProjectReferences);
+			Assert.NotNull(project!.Condition);
+			Assert.Equal("condition-value", project.Condition!.Expression);
+		}
+	}
+
+	public partial class ProjectTests
+	{
+		[Fact]
+		public void AllPropertyGroupsShouldBeRead()
+		{
+			// Arrange
+			var xml = new ProjectBuilder()
+				.WithPropertyGroup()
+				.WithProperty("Property1", "1")
+				.WithPropertyGroup()
+				.WithProperty("Property2", "2")
+				.BuildXml();
+
+			// Act
+			var sut = new Project(xml);
+
+			// Assert
+			Assert.Equal(2, sut.PropertyGroups.Count);
+			var propertyNames = sut.PropertyGroups.SelectMany(x => x.Properties).Select(x => x.Name).ToList();
+			Assert.Contains("Property1", propertyNames);
+			Assert.Contains("Property2", propertyNames);
+		}
+
+		[Fact]
+		public void ConditionOfPropertyGroupShouldContainExpressionWhenItExists()
+		{
+			// Arrange
+			var xml = new ProjectBuilder()
+				.WithPropertyGroup("condition-value")
+				.WithProperty("ExistingProperty", "value")
+				.BuildXml();
+
+			// Act
+			var sut = new Project(xml);
+
+			// Assert
+			var condition = Assert.Single(sut.PropertyGroups)!.Condition;
+			Assert.NotNull(condition);
+			Assert.Equal("condition-value", condition!.Expression);
+		}
+
+		[Fact]
+		public void FindPropertyByNameShouldNotFindNonMatchingProperties()
+		{
+			// Arrange
+			var xml = new ProjectBuilder()
+				.WithPropertyGroup()
+				.WithProperty("Property", "1")
+				.WithPropertyGroup()
+				.WithProperty("Property2", "2")
+				.WithPropertyGroup()
+				.WithProperty("Property", "2")
+				.BuildXml();
+
+			var sut = new Project(xml);
+
+			// Act
+			var actual = sut.FindPropertyByName("Property");
+
+			// Assert
+			Assert.Equal(2, actual.Count());
+		}
+
+		[Fact]
+		public void FindPropertyByNameShouldReturnAllFoundProperties()
+		{
+			// Arrange
+			var xml = new ProjectBuilder()
+				.WithPropertyGroup()
+				.WithProperty("Property", "1")
+				.WithPropertyGroup()
+				.WithProperty("Property", "2")
+				.BuildXml();
+
+			var sut = new Project(xml);
+
+			// Act
+			var actual = sut.FindPropertyByName("Property");
+
+			// Assert
+			Assert.Equal(2, actual.Count());
+		}
+
+		[Fact]
+		public void GroupOfPropertyShouldHaveCorrectReference()
+		{
+			// Arrange
+			var sut = new ProjectBuilder()
+				.WithPropertyGroup()
+				.WithProperty("ExistingProperty", "value").Build();
+
+			// Act
+			var actual = sut.PropertyGroups.Single().Properties.Single();
+
+			// Assert
+			Assert.Same(sut.PropertyGroups.Single(), actual.Group);
+		}
+
+		[Fact]
+		public void PropertyValueShouldBeNullForEmptyProject()
+		{
+			// Arrange
+			var xml = new XDocument();
+			var sut = new Project(xml);
+
+			// Act
+			var actual = sut.PropertyValue("property");
+
+			// Assert
+			Assert.Null(actual);
+		}
+
+		[Fact]
+		public void PropertyValueShouldBeNullWhenNotFound()
+		{
+			// Arrange
+			var sut = new ProjectBuilder()
+				.WithPropertyGroup()
+				.WithProperty("ExistingProperty", "value").Build();
+
+			// Act
+			var actual = sut.PropertyValue("NonExistingProperty");
+
+			// Assert
+			Assert.Null(actual);
+		}
+
+		[Fact]
+		public void PropertyValueShouldHaveCorrectValueWhenFound()
+		{
+			// Arrange
+			var sut = new ProjectBuilder()
+				.WithPropertyGroup()
+				.WithProperty("ExistingProperty", "value").Build();
+
+			// Act
+			var actual = sut.PropertyValue("ExistingProperty");
+
+			// Assert
+			Assert.Equal("value", actual);
 		}
 	}
 }
